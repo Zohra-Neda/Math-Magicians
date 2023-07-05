68c09f3aac388a91d103e7506c1175a27348ba34
zohra-neda <zohra.neda@gmail.com>
2023-07-05 16:48:20 +0430

    Implement click event handler

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import calculate from '../logic/calculate';
 import Container from './CalcContainer';
 import Buttons from './Buttons';
 
-const Calculator = () => (
-  <div className="main">
-    <Container />
-    <Buttons />
-  </div>
-);
+const Calculator = () => {
+  const [obj, setObject] = useState({
+    total: 0,
+    next: null,
+    operation: null,
+  });
+
+  const handleButtonClick = (e) => {
+    setObject(calculate(obj, e.target.innerHTML));
+  };
+
+  return (
+    <div className="main">
+      <Container total={obj.total} operation={obj.operation} next={obj.next} />
+      <Buttons handleButtonClick={handleButtonClick} />
+    </div>
+  );
+};
 
 export default Calculator;
