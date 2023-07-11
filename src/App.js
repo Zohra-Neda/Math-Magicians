d5f0a35ec1e6644ebd5193ff8eb3d0197ab8d077
zohra-neda <zohra.neda@gmail.com>
2023-07-11 18:18:43 +0430

    Add react routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+/*eslint-disable*/
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
 import Calculator from './components/Calculator';
 import Quote from './components/Quote';
 
 function App() {
   return (
-    <div>
-      <Calculator />
-      <Quote />
-    </div>
+    <>
+      <header className='header'>
+      <h1 className="header-title">Math Magicians</h1>
+        <nav className='nav'>
+          <ul className='ul'>
+            <li className='nav-link'><Link to='/'>Home |</Link></li>
+            <li className='nav-link'><Link to='/calculator'>Calculator |</Link></li>
+            <li className='nav-link'><Link to='/quotes'>Quote |</Link></li>
+          </ul>
+        </nav>
+      </header>
+      <main className='main-container'>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/calculator' element={<Calculator/>} />
+          <Route path='/quotes' element={<Quote/>} />
+        </Routes>
+      </main>
+    </>
   );
 }
 
